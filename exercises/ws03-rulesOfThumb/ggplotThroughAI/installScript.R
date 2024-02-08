032944f7854f9c769e9f33e6f44ffc4e3ae3ff86
hendrikknoche <hk@create.aau.dk>
2024-02-08 12:33:07 +0100

    added info about API key

--- exercises/ws03-rulesOfThumb/ggplotThroughAI/installScript.R
+++ exercises/ws03-rulesOfThumb/ggplotThroughAI/installScript.R
@@ -5,3 +5,6 @@ library(remotes)
 #voice input package heyshiny
 install_github("jcrodriguez1989/heyshiny", dependencies = TRUE)
 install_github("gexijin/RTutor")
+library(RTutor)
+RTutor::run_app()
+#it needs an API key from OpenAI to work
