6d79739efe291241aef483d0d7e28f260967b785
hendrikknoche <hk@create.aau.dk>
2024-04-16 16:09:12 +0200

    added more starter code

--- exercises/ws08-networkData/RES-QnetworkVis.R
+++ exercises/ws08-networkData/RES-QnetworkVis.R
@@ -1,5 +1,28 @@
 library(ggraph)
 library(igraph)
+#load the data
 nodes<-read.csv("data/RESQ-nodes.csv")
-edges<-read.csv("RESQ-edges.csv")
+edges<-read.csv("data/RESQ-edges.csv")
+
+# create the net object with edges and nodes
 net<-graph_from_data_frame(d=edges, vertices=nodes, directed=T)
+
+#example plot
+plot(net, edge.arrow.mode=2, layout=do.call("layout_as_star", list(net)) , main="layout_in_circle", edge.arrow.size = 0.2)
+
+#other options
+#  "layout_as_star"
+#  "layout_components"
+#  "layout_in_circle"
+#  "layout_nicely"
+#  "layout_on_grid"
+#  "layout_on_sphere"
+#  "layout_randomly"
+#  "layout_with_dh"
+#  "layout_with_drl"
+#  "layout_with_fr"
+#  "layout_with_gem"
+#  "layout_with_graphopt"
+#  "layout_with_kk"
+#  "layout_with_lgl"
+#  "layout_with_mds"
